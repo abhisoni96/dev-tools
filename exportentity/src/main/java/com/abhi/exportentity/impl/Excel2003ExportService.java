eadd99d6513f2d0124f10f8f8083395b963caf50
Abhishek Soni <abhisoni96@users.noreply.github.com>
2016-11-28 14:54:13 +0530

    Excel data should be written from first row.

--- exportentity/src/main/java/com/abhi/exportentity/impl/Excel2003ExportService.java
+++ exportentity/src/main/java/com/abhi/exportentity/impl/Excel2003ExportService.java
@@ -57,7 +57,7 @@ public class Excel2003ExportService<E> extends AbstractFlatDataExportService<E> 
 
 		@Override
 		public void init() {
-			this.row = this.workSheet.createRow(this.workSheet.getLastRowNum() + 1);
+			this.row = this.workSheet.createRow(this.workSheet.getPhysicalNumberOfRows());
 		}
 
 		@Override
