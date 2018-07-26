6c761abf3c78eb1a767f7fc62d62f744426c3a19
abhisoni96 <abhi_soni@live.in>
2018-07-25 23:27:06 -0400

    Added basic Performance Log Analyzer structure
    Signed-off-by: abhisoni96 <abhi_soni@live.in>

---
+++ perfmetricstool/src/main/java/com/abhi/tools/perfmetrics/App.java
@@ -0,0 +1,13 @@
+package com.abhi.tools.perfmetrics;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
